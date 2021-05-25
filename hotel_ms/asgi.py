a87013730e763c13d0ab22f4bcaa88410e3514b9
Ceasar15 <ceasarkwadwo@gmail.com>
2021-05-25 20:06:00 +0000

    added main app

--- hotel_ms/asgi.py
+++ hotel_ms/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Hotel_MS project.
+ASGI config for hotel_ms project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Hotel_MS.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hotel_ms.settings')
 
 application = get_asgi_application()
--- hotel_ms/wsgi.py
+++ hotel_ms/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Hotel_MS project.
+WSGI config for hotel_ms project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Hotel_MS.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hotel_ms.settings')
 
 application = get_wsgi_application()

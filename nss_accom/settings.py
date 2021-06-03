3dbfbfd29096cc5d701b3e3ae3efe12088f57010
Ceasar15 <ceasarkwadwo@gmail.com>
2021-06-03 23:21:01 +0000

    settings file

--- nss_accom/settings.py
+++ nss_accom/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ.get('NAME'),
-        'USER': os.environ.get('USER'),
+        'USER': 'bookmarks',
         'PASSWORD': os.environ.get('PASSWORD'),
         'HOST': os.environ.get('HOST'),
         'PORT': '',

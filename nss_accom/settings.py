a3be67f045c86cf5139aa58a7d3dd1e80c93aeb3
Ceasar15 <ceasarkwadwo@gmail.com>
2021-06-11 12:07:21 +0000

    chnages

--- nss_accom/settings.py
+++ nss_accom/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['nss-accom.herokuapp.com', 'localhost', '0.0.0.0']
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '0.0.0.0']
 
 
 # Application definition

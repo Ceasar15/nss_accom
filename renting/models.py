50de69d9f23dbd7d3e6c42d70df377dfade3ac3b
Ceasar15 <ceasarkwadwo@gmail.com>
2021-06-23 18:22:28 +0000

    search feature

--- renting/models.py
+++ renting/models.py
@@ -1,13 +1,13 @@
-from django.db import models, Q
+from django.db import models
 from django.contrib.auth.models import User
-from datetime import date
+from django.db.models import Q
 import django_filters
 
 # Create your models here.
 
 STATE_CHOICES = [
 
-	('Greater Accra Region','Greater Accra Region'),
+    ('Greater Accra Region','Greater Accra Region'),
 ('Ashanti Region','Ashanti Region'),
 ('Western North Region','Western North Region'),
 ('Western Region','Western Region'),
@@ -26,34 +26,34 @@ STATE_CHOICES = [
 ]
 
 HH_FIELD_CHOICES = (
-		('Pr','Private'),
-		('Sh','Shared'),
-		('No','Not Available'),
-	)
+        ('Pr','Private'),
+        ('Sh','Shared'),
+        ('No','Not Available'),
+    )
 
 FIELD_CHOICES =(
-		('Y','Yes'),
-		('N','No')
+        ('Y','Yes'),
+        ('N','No')
 )
 
 GENDER_PREF = (
-	('M','MALE'),
-	('F','FEMALE'),
-	('O','OTHERS')
+    ('M','MALE'),
+    ('F','FEMALE'),
+    ('O','OTHERS')
 )
 
 ROOMS = (
-	('zero',0),
-	('one',1),
-	('two',2),
-	('three',3),
-	('three_plus',4)
+    ('zero',0),
+    ('one',1),
+    ('two',2),
+    ('three',3),
+    ('three_plus',4)
 
 )
 
 
 def house_images(instance, filename):
-	return f'user_{instance.nrh.user.username}/{filename}'
+    return f'user_{instance.nrh.user.username}/{filename}'
 
 
 class NewRentalHouse(models.Model):
@@ -75,61 +75,54 @@ class NewRentalHouse(models.Model):
     rent = models.PositiveIntegerField(default=100)
 
 class HouseImages(models.Model):
-	images = models.ImageField(upload_to=house_images, blank=True)
-	nrh = models.ForeignKey(NewRentalHouse, on_delete=models.CASCADE)
+    images = models.ImageField(upload_to=house_images, blank=True)
+    nrh = models.ForeignKey(NewRentalHouse, on_delete=models.CASCADE)
 
 
 
 class HouseHas(models.Model):
 
-	bedroom = models.CharField(max_length=11, choices=ROOMS)
-	kitchen = models.CharField(max_length=2, choices=HH_FIELD_CHOICES)
-	bathroom = models.CharField(max_length=2, choices=HH_FIELD_CHOICES)
-	living_room = models.CharField(max_length=2, choices=HH_FIELD_CHOICES)
-	toilet = models.CharField(max_length=2, choices=HH_FIELD_CHOICES)
-	balcony = models.CharField(max_length=2, choices=HH_FIELD_CHOICES)
-	parking = models.CharField(max_length=2, choices=FIELD_CHOICES)
-	nrh = models.OneToOneField(NewRentalHouse, on_delete=models.CASCADE) 
+    bedroom = models.CharField(max_length=11, choices=ROOMS)
+    kitchen = models.CharField(max_length=2, choices=HH_FIELD_CHOICES)
+    bathroom = models.CharField(max_length=2, choices=HH_FIELD_CHOICES)
+    living_room = models.CharField(max_length=2, choices=HH_FIELD_CHOICES)
+    toilet = models.CharField(max_length=2, choices=HH_FIELD_CHOICES)
+    balcony = models.CharField(max_length=2, choices=HH_FIELD_CHOICES)
+    parking = models.CharField(max_length=2, choices=FIELD_CHOICES)
+    nrh = models.OneToOneField(NewRentalHouse, on_delete=models.CASCADE) 
 
 region = models.CharField(max_length=100, choices=STATE_CHOICES)
 
 class Amenities(models.Model):
 
-	bed = models.CharField(max_length=1, choices=FIELD_CHOICES)
-	furnished = models.CharField(max_length=1, choices=FIELD_CHOICES)
+    bed = models.CharField(max_length=1, choices=FIELD_CHOICES)
+    furnished = models.CharField(max_length=1, choices=FIELD_CHOICES)
 
-	nrh = models.OneToOneField(NewRentalHouse, on_delete=models.CASCADE)
+    nrh = models.OneToOneField(NewRentalHouse, on_delete=models.CASCADE)
 
 
 class PreferredTenant(models.Model):
-	gender = models.CharField(max_length=1, choices=GENDER_PREF)
-	nrh = models.OneToOneField(NewRentalHouse, on_delete=models.CASCADE)
+    gender = models.CharField(max_length=1, choices=GENDER_PREF)
+    nrh = models.OneToOneField(NewRentalHouse, on_delete=models.CASCADE)
 
 
 class Rules(models.Model):
-	smoking_allowed = models.CharField(max_length=1, choices=FIELD_CHOICES)
-	musical_instrument = models.CharField(max_length=1, choices=FIELD_CHOICES)
+    smoking_allowed = models.CharField(max_length=1, choices=FIELD_CHOICES)
+    musical_instrument = models.CharField(max_length=1, choices=FIELD_CHOICES)
 
-	nrh = models.OneToOneField(NewRentalHouse, on_delete=models.CASCADE)
+    nrh = models.OneToOneField(NewRentalHouse, on_delete=models.CASCADE)
 
-	# class Meta:
-	# 	unique_together = ()
 
 
 class SearchFilter(django_filters.FilterSet):
-    city = django_filters.CharFilter(method='custom_filter')		
-	
-
-	class Meta:
-		
-		model = NewRentalHouse 
-		fields={
-			'rent': ['gt', 'lt'],
-		}
-	
-	def custom_filter(self, queryset, name, value):
-    		return NewRentalHouse.objects.filter(
-				Q(city__icontains=value) | 
-				Q(area__icontains=value) | 
-				Q(street_address__icontains=value)
-			)
+    city = django_filters.CharFilter(method='custom_filter')
+    class Meta:
+        model = NewRentalHouse
+        fields = {
+            'rent': ['gt', 'lt']
+        }
+    
+    def custom_filter(self, queryset, name, value):
+        return NewRentalHouse.objects.filter(
+            Q(city__icontains=value) | Q(area__icontains=value) | Q(street_address__icontains=value)  
+        )

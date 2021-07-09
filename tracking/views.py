c11b73907843a42760590d2a4aebf5f42dcdeff7
larry king <lw1079030@gmail.com>
2021-07-09 00:20:15 -0700

    first commit

--- tracking/views.py
+++ tracking/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'tracking/index.html')
+    return render(request, 'tracking/indexxxx.html')

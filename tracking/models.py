b506a04d5d0b87ef4c0b008e30072c6790db75b5
Ceasar15 <ceasarkwadwo@gmail.com>
2021-07-12 21:00:19 +0000

    tracking backend

--- tracking/models.py
+++ tracking/models.py
@@ -3,3 +3,41 @@ from django.contrib.auth.models import User
 from django.db.models import Q
 import django_filters
 
+
+GENDER_PREF = (
+    ('M','MALE'),
+    ('F','FEMALE'),
+    ('O','OTHERS')
+)
+
+LEVEL_CHOICES = (
+    ('M','MALE'),
+    ('F','FEMALE'),
+    ('O','OTHERS')
+)
+
+class NewStudentData(models.Model):
+    index_number = models.CharField(primary_key=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    gender = models.CharField(max_length=10, choices=GENDER_PREF)
+    room_number = models.CharField(max_length=10)
+    course = models.CharField(max_length=50)
+    level = models.CharField(max_length=50, choices=LEVEL_CHOICES)
+    mobile_number = models.CharField(max_length=50)
+    date_added = models.DateTimeField()
+    check_in = models.BooleanField()
+
+
+class StudentImages(models.Model):
+    images = models.ImageField(upload_to=house_images, blank=True)
+    nrh = models.ForeignKey(NewStudentData, on_delete=models.CASCADE)
+
+
+class PostAnnouncement(models.Model):
+    announcement_id = models.AutoField()
+    announcement_title = models.CharField(max_length=100)
+    announcement_body = models.TextField()
+    date_submitted = models.DateField()
+    time_submitted = models.TimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)

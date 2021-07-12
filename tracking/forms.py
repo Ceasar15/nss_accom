dce56fc24e70b8c0ae36daab28887ecb8a2b2f82
Ceasar15 <ceasarkwadwo@gmail.com>
2021-07-12 21:19:19 +0000

    tracking backend

--- tracking/forms.py
+++ tracking/forms.py
@@ -1,2 +1,13 @@
 from django import forms
-from .models import GENDER_PREF, LEVEL_CHOICES, VISITOR_STATUS
+from .models import GENDER_PREF, LEVEL_CHOICES, VISITOR_STATUS, NewStudent, NewVisitor, PostAnnouncement, StudentImages
+
+
+
+
+
+class HouseImagesForm(forms.ModelForm):
+	images = forms.ImageField(widget=forms.ClearableFileInput(attrs={'class':'file-input','multiple':True}), required=False)
+
+	class Meta:
+		model = StudentImages
+		fields = ['images']

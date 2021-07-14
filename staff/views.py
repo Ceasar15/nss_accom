c57d2339caeda96a18388c5b9e4ab15cc63f8741
Ceasar15 <ceasarkwadwo@gmail.com>
2021-07-14 17:02:49 +0000

    wrote staff backend

--- staff/views.py
+++ staff/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+def loginStudent(request):
+    
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password =request.POST.get('password')
 
-# Create your views here.
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:	
+            login(request, user)	
+
+            if request.user:
+                return redirect('tracking:studentDashboard')
+            else:
+                return redirect('tracking:studentSubmitComplaint')
+
+        else:
+            messages.info(request, 'ID OR Password is incorrect')		
+
+    context = {}
+
+    return render(request, 'tracking/login.html', context)
+

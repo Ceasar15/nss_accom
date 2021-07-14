210483629514ac1badc6128a3914edfa9d0271f3
Ceasar15 <ceasarkwadwo@gmail.com>
2021-07-14 16:38:10 +0000

    student backend

--- student/forms.py
+++ student/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
 from django.db import transaction
-
+from student.models import CHOICES
 
 class StudentRegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)

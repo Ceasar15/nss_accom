5e48c6a9d8abecfb0b464999546f10902917e551
Ceasar15 <ceasarkwadwo@gmail.com>
2021-07-15 10:08:19 +0000

    backend changes

--- users/views.py
+++ users/views.py
@@ -29,13 +29,18 @@ def Studentregister(request):
 
     if request.method == 'POST':
         form = StudentRegisterForm(request.POST)
-        # user_contact_form = UserContactFrom(request.POST)
-        if form.is_valid():
-            user_contact_form = Typed()
-            user_contact_form.user_group = request.POST.get('user_group')
-            user_contact_form.phone_no = request.POST.get('phone_no')
+        user_contact_form = UserContactFrom(request.POST)
+        if all((form.is_valid(), user_contact_form.is_valid())):
+
+            user_contact_form.save(commit=False)
+            user_contact_form.user = form 
             user_contact_form.save()
-            form.save()
+            form = form.save()
+            
+            # user_contact_form.user_group = request.POST.get('user_group')
+            # user_contact_form.phone_no = request.POST.get('phone_no')
+            # user_contact_form.save()
+            # form.save()
             return redirect('student:studentDashboard')
 
     context = {

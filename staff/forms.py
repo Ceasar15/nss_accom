4b1bc9ee46234ea8ad4a1116eafe5aabeaf837da
Ceasar15 <ceasarkwadwo@gmail.com>
2021-07-15 12:38:37 +0000

    backend changes

---
+++ staff/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from django.forms import fields
+from .models import GENDER_PREF, LEVEL_CHOICES, VISITOR_STATUS, NewStudent, NewVisitor, PostAnnouncement, StudentImages
+
+
+class PostAnnoumcementForm(forms.ModelForm):
+    announcement_title = forms.CharField(max_length=100)
+    announcement_body = forms.Textarea()
+    # user_group  = forms.CharField(max_length=30, widget=forms.Select(choices= CHOICES))
+    
+    class Meta:
+        model = PostAnnouncement
+        fields = (
+            'announcement_title',
+            'announcement_body'
+        )
+
+class StudentImagesForm(forms.ModelForm):
+    images = forms.ImageField(widget=forms.ClearableFileInput(attrs={'class':'file-input','multiple':True}), required=False)
+
+    class Meta:
+        model = StudentImages
+        fields = ['images']
--- staff/views.py
+++ staff/views.py
@@ -4,6 +4,8 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
+from .models import PostAnnouncement
+from .forms import PostAnnoumcementForm
 
 def loginStaff(request):
     
@@ -27,41 +29,38 @@ def loginStaff(request):
 
     return render(request, 'staff/login_staff.html', context)
 
-<<<<<<< HEAD
-@login_required(login_url='/loginStaff')
-||||||| ed5ec9d
-=======
-
->>>>>>> 267ab626ae90cefc7d1800b7691d4e1fd8114605
 def staffDashboard(request):
     return render(request, 'staff/staff_dashboard.html')
 
-<<<<<<< HEAD
-
-@login_required(login_url='/loginStaff')
-||||||| ed5ec9d
-=======
-
->>>>>>> 267ab626ae90cefc7d1800b7691d4e1fd8114605
 def staff_addNewStudent(request):
     return render(request, 'staff/add_new_student.html')
 
-<<<<<<< HEAD
-
-@login_required(login_url='/loginStaff')
-||||||| ed5ec9d
-=======
-
->>>>>>> 267ab626ae90cefc7d1800b7691d4e1fd8114605
 def staff_addNewVisitor(request):
     return render(request, 'staff/add_new_visitor.html')
 
-<<<<<<< HEAD
-
-@login_required(login_url='/loginStaff')
-||||||| ed5ec9d
-=======
-
->>>>>>> 267ab626ae90cefc7d1800b7691d4e1fd8114605
 def staff_postAnnouncement(request):
-    return render(request, 'staff/post_announcement.html')
+    p_form = PostAnnoumcementForm()
+
+    if request.method == 'POST':
+        p_form = PostAnnoumcementForm(request.POST, instance=request.user)
+        
+        if p_form.is_valid():
+            
+            p_form.save()
+            
+            messages.success(request, f'Your Announcement has been Updated Successfully')
+            return redirect('staff:staffDashboard')
+    
+    else:
+        p_form=PostAnnoumcementForm()
+        context={
+            'p_form': p_form
+            }
+        
+        return render(request, 'staff/post_announcement.html', context)
+    
+    context={
+            'p_form': p_form
+            }
+        
+    return render(request, 'staff/post_announcement.html', context)

50dbc2b5243e448ec00300f940071104fe366476
Ceasar15 <ceasarkwadwo@gmail.com>
2021-07-15 01:39:19 +0000

    backend refactor

--- users/models.py
+++ users/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 from users.managers import UT
-
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 CHOICES = (
-	('student','Student'),
-	('staff','Staff'),
-	('landlord', 'Landlord'),
+	('student','student'),
+	('staff','staff'),
+	('landlord', 'landlord'),
 )
 
 
@@ -14,6 +15,7 @@ class UserType(models.Model):
 
 	user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='ut')
 	user_type = models.CharField(max_length=8, choices=CHOICES, default='owner')
+	# user_group = models.CharField(max_length=20, choices=CHOICES, default='student')
 
 
 	class Meta:
@@ -23,3 +25,13 @@ class UserType(models.Model):
 class ContactDetails(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	phone_no = models.IntegerField()
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()

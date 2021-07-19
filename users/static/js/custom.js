e2fc9511aa9d4b8c3ec9989c1461bc940f64dab3
larry king <lw1079030@gmail.com>
2021-07-18 21:00:33 -0700

    other

--- users/static/js/custom.js
+++ users/static/js/custom.js
@@ -2,8 +2,10 @@
 
  document.getElementById('id_complaint_type').onchange = function () {
     if(this.value == 'Other') {
+        document.getElementById('id_complaint_type').setAttribute("disabled", "disabled")
         document.getElementById('id_other_complaint_type').removeAttribute("disabled")
     } else {
+        document.getElementById('id_complaint_type').removeAttribute("disabled")
         document.getElementById('id_other_complaint_type').setAttribute("disabled", "disabled")
     }
 

6e5ec849db789fca4bdaaa68831856628720e3ad
larry king <lw1079030@gmail.com>
2021-07-18 23:42:00 -0700

    other

--- users/static/js/custom.js
+++ users/static/js/custom.js
@@ -1,5 +1,4 @@
 // showing the other complaint text field only when other complaint is selected.
-
  document.getElementById('id_complaint_type').onchange = function () {
     if(this.value == 'Other') {
         document.getElementById('id_complaint_type').setAttribute("disabled", "disabled")
@@ -9,6 +8,28 @@
         document.getElementById('id_other_complaint_type').setAttribute("disabled", "disabled")
     }
 
+}
+
+
+
+
+/* disabling and enabling the visitor id field based on visitor status.
+document.getElementById('id_visiting_status').onchange = function() {
+    if(this.value == 'Yes') {
+        document.getElementById('id_visitor_id').disabled = false
+    } else {
+        document.getElementById('id_visitor_id').disabled = true
+    }
+
+}
+
+*/
+
+
+
+function checkOption(obj) {
+    var input = document.getElementById("id_visitor_id");
+    input.disabled = obj.value == "No";
 }
 
 

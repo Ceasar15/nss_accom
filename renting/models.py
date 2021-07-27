db1a3f2a88421c037da06652a8827da61c42bf51
Ceasar15 <ceasarkwadwo@gmail.com>
2021-07-27 12:44:16 +0000

    mee

--- renting/models.py
+++ renting/models.py
@@ -68,7 +68,7 @@ class NewRentalHouse(models.Model):
     rent = models.PositiveIntegerField(default=100)
 
 class HouseImages(models.Model):
-    images = models.ImageField(upload_to=house_images, blank=True)
+    images = models.FileField(upload_to=house_images, blank=True)
     nrh = models.ForeignKey(NewRentalHouse, on_delete=models.CASCADE)
 
     def images(self):

e1f565fc52b1aa616e2e33f38166c1c5592b9fa8
Ceasar15 <ceasarkwadwo@gmail.com>
2021-07-28 18:42:13 +0000

    backend

--- renting/views.py
+++ renting/views.py
@@ -738,7 +738,24 @@ def landlordViewHouseDetails(request, id):
 
 # the page where the student can view the details of the ad.
 def studentViewHouseDetails(request, id):
-    return render(request, 'renting/student_view_ad_details.html')
+    if request.user.is_authenticated:
+        try:
+            nrh_obj = NewRentalHouse.objects.get(pk=id)
+            r_hh = HouseHas.objects.get(nrh=nrh_obj)
+            am = Amenities.objects.get(nrh=nrh_obj)
+            pt = PreferredTenant.objects.get(nrh=nrh_obj)
+            rl = Rules.objects.get(nrh=nrh_obj)
+            imgs = HouseImages.objects.filter(nrh=nrh_obj)
+            for img in imgs:
+                print(img.images)
+        except:
+            nrh_obj = None
+        if nrh_obj:
+            return render(request, 'renting/landlord_view_own_ads_details.html', locals())
+    elif request.user.is_anonymous:
+        modl='true'
+        return render(request, 'renting/student_view_ad_details.html', locals())
+        
 
 
 

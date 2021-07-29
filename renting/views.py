ff9add23c913a7d5751cfc223237dff1b6c16587
Ceasar15 <ceasarkwadwo@gmail.com>
2021-07-29 15:00:33 +0000

    changes

--- renting/views.py
+++ renting/views.py
@@ -721,7 +721,9 @@ def studentViewLandlordDetails(request, id):
     landlord = User.objects.get(id=id)
     profile = Profile.objects.get(user_id=id)
     typed = Typed.objects.get(user_id_id=id)
-
+    print(landlord.location)
+    # print(landlord.profile_image)
+    
     contact_landlord = ContactLandlordForm(request.POST)
     if request.method == 'POST':
         if contact_landlord.is_valid():

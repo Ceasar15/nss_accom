cae6465ac80a18a455deab7fee805aaccf870c7c
Ceasar15 <ceasarkwadwo@gmail.com>
2021-08-09 13:53:01 +0000

    deployment

--- tracking/views.py
+++ tracking/views.py
@@ -1,1 +1,12 @@
+from django.shortcuts import render
+from django.contrib.auth import logout
+
+
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'tracking/indexxxx.html')
+
+
 

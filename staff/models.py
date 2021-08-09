71784f0968b1b259c30dddd1dba77487cf60c690
Ceasar15 <ceasarkwadwo@gmail.com>
2021-08-09 13:42:57 +0000

    deployment

--- staff/models.py
+++ staff/models.py
@@ -1,11 +1,6 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from django.db.models import Q
-import django_filters
-
 from django.utils import timezone
 
 GENDER_PREF = (

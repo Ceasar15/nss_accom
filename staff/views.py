25562ea4c80ddbd9f6a7b176c80511ed9b4a0b34
larry king <lw1079030@gmail.com>
2021-08-12 02:49:26 -0700

    complaints

--- staff/views.py
+++ staff/views.py
@@ -254,3 +254,9 @@ def solved(request, complaint_id):
     complaint.complaint_status = 'RESOLVED'
     complaint.save()
     return redirect('staff:staffViewAllComplaints')
+
+
+
+# datatables tutorial.
+def dataTables(request):
+    return render (request, 'staff/data_tables.html')
